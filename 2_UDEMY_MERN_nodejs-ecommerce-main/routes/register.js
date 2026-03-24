0bfccee93afe36036e027e11ba40c222f4903f9d
rajeshthakur421 <rajeshthakur421@gmail.com>
2026-03-24 10:27:51 +0530

    Changes

--- 2_UDEMY_MERN_nodejs-ecommerce-main/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { create } = require('../controllers/register');
-
-router.post('/register/create/', create);
-
-module.exports = router;
